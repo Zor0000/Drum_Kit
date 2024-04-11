880d337f2479e944109abccf77a6f86d87c790ce
Neeraj Chormale <neerajchormale39@gmail.com>
2024-04-12 00:04:12 +0530

    Added shadows buttons
    Added shadows to each buttons which will activate when the drum is pressed or clicked

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for (var i = 0; i<numberOfDrumButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
              
     });
 }
@@ -12,6 +13,7 @@ for (var i = 0; i<numberOfDrumButtons; i++){
 //Detecting Keyboard press
 document.addEventListener("keypress",function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -52,4 +54,12 @@ function makeSound(key){
          break;
 
     }
+}
+
+function buttonAnimation(currentkey){
+    var activeButton = document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
 }
